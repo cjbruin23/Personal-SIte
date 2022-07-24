724bfaa620f2a5946747db6eabd8e3616cdf98a2
Colin Bruin <colinjb23@gmail.com>
2022-07-24 14:52:20 -0600

    setting up eslint and prettier

--- src/core/app/App.tsx
+++ src/core/app/App.tsx
@@ -3,13 +3,12 @@ import Header from '../header/header';
 import Body from '../body/body';
 
 function App() {
-
   return (
     <div>
       <Header></Header>
       <Body></Body>
     </div>
-  )
+  );
 }
 
 export default App;
--- src/core/body/body.tsx
+++ src/core/body/body.tsx
@@ -1,10 +1,12 @@
 function Body() {
-    return (
-        <div id="body" className='container mx-auto'>
-        <p>Welcome to my perosnal site. This is to showcase my work, blogs and
-        skills that I have accumulated over the years</p>
-      </div>
-    )
+  return (
+    <div id='body' className='container mx-auto'>
+      <p>
+        Welcome to my perosnal site. This is to showcase my work, blogs and skills that I have
+        accumulated over the years
+      </p>
+    </div>
+  );
 }
 
 export default Body;
