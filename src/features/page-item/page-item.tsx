f40891e1fea3cb55cd48806a1d97b1a2f15e9bfa
Colin Bruin <colinjb23@gmail.com>
2022-08-15 17:39:48 -0600

    adding conditional routing to page-item; adding InstagramAPI page

--- src/features/page-item/page-item.tsx
+++ src/features/page-item/page-item.tsx
@@ -1,5 +1,25 @@
+import { Fragment } from "react";
+import { useParams } from "react-router-dom";
+import InstagramAPI from "../instagram-api/instagram-api";
+
 function PageItem() {
-  return <div>Page Item</div>;
+  const params = useParams();
+  console.log('params', params)
+  return (
+      
+    <Fragment>
+      <div>Page Item</div>
+      {(() => {
+        switch (params.id) {
+          case '1':
+            return <InstagramAPI></InstagramAPI>
+          
+          default:
+            return null
+        }
+      })()}
+    </Fragment>
+  );
 }
 
 export default PageItem;
