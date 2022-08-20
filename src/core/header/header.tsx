ab2be1f14269477173c2544d86eaf607b40b9248
cjbruin23 <colinjb23@gmail.com>
2022-08-20 12:45:43 -0600

    Dev to Master (#19)
    * Dev to Master (#13) (#16)
    
    Fixing dev to master merge errors
    
    * Home page fix (#18)
    
    *Reformulated tags and took out routing for now
    
    *Gave h1 a global style

--- src/core/header/header.tsx
+++ src/core/header/header.tsx
@@ -8,7 +8,7 @@ function Header() {
         className='text-center font-headerText mb-5 bg-cover h-screen'
       >
         <div id='header-info' className='m-0 absolute top-2/4 left-2/4'>
-          <h1 className='text-white text-6xl mb-2'>Colin Bruin</h1>
+          <h1 className='text-white text-6xl mb-3'>Colin Bruin</h1>
           <p className='text-white text-2xl'>
             <i>
               A software engineer with passion for solving difficult problems in the
