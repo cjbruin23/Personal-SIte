50da4378e9592f0cde0b79e93c2a680a851af391
cjbruin23 <colinjb23@gmail.com>
2022-08-20 09:38:07 -0600

    Dev to Master (#13)
    * Create page item (#9)
    
    * adding in page-item and page-item-link
    
    * going to the proper link when clicking on page-item link
    
    * small change to readme regarding type vs interface
    
    * attempting to pass a function to page item, but was not able. i think I may need state to go ahead and store what API call is needed to be made
    
    * comment added for later
    
    * changing npm dev to start
    
    * adding conditional routing to page-item; adding InstagramAPI page
    
    * adding fetch
    
    * Blog feature (#10)
    
    * checking title
    
    * moved header to one row; spacing between nav items is better
    
    * adding a file
    
    * making changes to body on home page
    
    * adding image to website
    
    * adding static asset
    
    * Blog feature (#12)
    
    * checking title
    
    * moved header to one row; spacing between nav items is better
    
    * adding a file
    
    * making changes to body on home page
    
    * adding image to website
    
    * adding static asset
    
    * build is now adding image file to dist
    
    * adding type file

--- src/core/header/header.tsx
+++ src/core/header/header.tsx
@@ -1,7 +1,10 @@
+import Img from "../../images/rockies.jpg"
+
 function Header() {
   return (
     <header className=''>
-      <img src="src/images/rockies.jpg" alt="header rockies picture"></img>
+      <img src={Img} alt="header rockies picture"></img>
+
       <div className='text-center font-headerText text-3xl mb-5'>Colin Bruin</div>
     </header>
   );
