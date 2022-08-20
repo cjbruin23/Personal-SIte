0920563e7f63f4b3ce46ad48877a3f8477cb1d9d
cjbruin23 <colinjb23@gmail.com>
2022-08-20 12:27:58 -0600

    Home page fix (#14)
    Header image is the background of my name and description

--- src/core/header/header.tsx
+++ src/core/header/header.tsx
@@ -1,11 +1,24 @@
-import Img from "../../images/rockies.jpg"
+import './header.css';
 
 function Header() {
   return (
-    <header className=''>
-      <img src={Img} alt="header rockies picture"></img>
-
-      <div className='text-center font-headerText text-3xl mb-5'>Colin Bruin</div>
+    <header>
+      <div
+        id='header-container'
+        className='text-center font-headerText mb-5 bg-cover h-screen'
+      >
+        <div id='header-info' className='m-0 absolute top-2/4 left-2/4'>
+          <h1 className='text-white text-6xl mb-2'>Colin Bruin</h1>
+          <p className='text-white text-2xl'>
+            <i>
+              A software engineer with passion for solving difficult problems in the
+              web application space. Have worked on large enterprise applications
+              with a variety of technologies not limited to: Angular, NodeJS, AWS,
+              React, Postgres, DynamoDB, C#, ASP.NET, SQL Server.
+            </i>
+          </p>
+        </div>
+      </div>
     </header>
   );
 }
