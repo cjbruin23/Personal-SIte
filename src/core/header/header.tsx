8f6eaadc77a1609008a89b749248476e954c9584
Colin Bruin <colinjb23@gmail.com>
2022-08-20 09:56:23 -0600

    fixing header background image size

--- src/core/header/header.tsx
+++ src/core/header/header.tsx
@@ -1,9 +1,10 @@
 import Img from "../../images/rockies.jpg"
+import './header.css'
 
 function Header() {
   return (
-    <header className=''>
-      <img src={Img} alt="header rockies picture"></img>
+    <header>
+      <img src={Img} id="header-image" alt="header rockies picture"></img>
       <div className='text-center font-headerText text-3xl mb-5'>Colin Bruin</div>
     </header>
   );
