024a13123ca583f39d0aae6d63cd59cbf1f432bb
Colin Bruin <colinjb23@gmail.com>
2023-01-16 19:43:24 -0700

    adding color and size to header

--- src/core/header/header.tsx
+++ src/core/header/header.tsx
@@ -1,6 +1,8 @@
+import './header.css';
+
 function Header() {
   return (
-    <header className='flex-initial'>
+    <header id='header' className='flex-initial'>
       <div id='header-container' className='font-headerText mb-5'>
         <div id='header-info' className='ml-7 w-1/2'>
           <h1 className='text-6xl mb-3'>Colin Bruin</h1>
