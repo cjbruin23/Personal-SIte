53dbfcf41d9ba35c8416986be2782f2332b11417
cjbruin23 <colinjb23@gmail.com>
2024-08-27 09:36:03 -0700

    adding in logout and login functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
+import { useAuth0 } from '@auth0/auth0-react';
 import './App.css'
-import Header from './layouts/Header'
 
 function App() {
+  const { loginWithRedirect, isAuthenticated, logout } = useAuth0();
+
+  if (isAuthenticated) {
+    return (
+      <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+      Log Out
+    </button>
+    )
+  }
 
   return (
-    <>
-      <Header/>
-    </>
+    <button onClick={() => loginWithRedirect()}>Log In</button>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    
+    <Auth0Provider
+    domain="colinbruin.us.auth0.com"
+    clientId="FEooNzzSnAlc9vr0sfKZ55DI9yFjyyTq"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
+  </Auth0Provider>
   </React.StrictMode>,
 )
