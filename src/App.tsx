4cdb471e9df6f6dc2d365daa529daa7ceaf96f5f
cjbruin23 <colinjb23@gmail.com>
2024-09-02 12:41:20 -0700

    install axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import axios from "axios";
 import "./App.css";
 import Header from "./layouts/Header";
 
 function App() {
+    axios({
+        method: 'get',
+        url: 'http://localhost:8080'
+    }).then(res => {
+        console.log("res", res)
+    })
  return (
   <Header/>
  )
