b2d78361ca5ebb03185543958634176cc3a68358
cjbruin23 <colinjb23@gmail.com>
2024-09-02 15:31:44 -0700

    adding stories component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,33 @@
 import axios from "axios";
 import "./App.css";
 import Header from "./layouts/Header";
+import { useEffect } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import Stories from "./components/Stories";
 
 function App() {
+  const { isAuthenticated } = useAuth0();
+
+  useEffect(() => {
     axios({
-        method: 'get',
-        url: 'http://localhost:8080'
-    }).then(res => {
-        console.log("res", res)
+      method: "get",
+      url: import.meta.env.VITE_LOCALHOST_SERVER,
     })
- return (
-  <Header/>
- )
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log("error connection to server", err.response);
+      });
+  }, []);
+
+  return (
+    <>
+      <Header />
+
+      {isAuthenticated ? <Stories /> : <div></div>}
+    </>
+  );
 }
 
 export default App;
